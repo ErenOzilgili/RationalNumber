87afb1ab8f0c1eed82cb9d738d3a57b96fcff754
ErenOzilgili <erenozilgili@gmail.com>
2024-02-08 11:33:43 +0300

    -Added public service methods getNumerator(), getDenominator();
    -Added private function greatestCommonDivisor();
    -Finally added a constructor which takes int arguments called numerator and denominator

--- RationalNumber.java
+++ RationalNumber.java
@@ -1,1 +1,51 @@
+public class RationalNumber{
 
+    private int numerator;
+    private int denominator;
+
+    public RationalNumber(int numerator, int denominator){
+        if(denominator == 0){
+            this.denominator = 1;
+
+        }
+        else{
+            if(denominator < 0){
+                this.denominator = -1 * denominator;
+                this.numerator = -1 * numerator;
+            }
+
+        }
+
+        int gcd = greatestCommonDivisor(this);
+
+        this.numerator /= gcd;
+        this.denominator /= gcd;
+
+    }
+
+    private int greatestCommonDivisor(RationalNumber rationalMain){
+        int min = Math.abs(Math.min(rationalMain.getNumerator(), rationalMain.getDenominator()));
+        int max = Math.abs(Math.max(rationalMain.getNumerator(), rationalMain.getDenominator()));
+
+        while(max % min != 0 || min != 1){
+            int temp = min;
+            min = max % min;
+            max = temp;
+
+        }
+
+        return min;
+
+    }
+
+    public int getNumerator(){
+        return this.numerator;
+
+    }
+
+    public int getDenominator(){
+        return this.denominator;
+        
+    }
+
+}
