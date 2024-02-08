32bc1507c28bbaa6e247d6d340197a7961f4c1dc
ErenOzilgili <erenozilgili@gmail.com>
2024-02-08 11:53:40 +0300

    -Comments added to RationalNumber class

--- RationalNumber.java
+++ RationalNumber.java
@@ -1,8 +1,19 @@
 public class RationalNumber{
 
+    /*
+     * -Numerator holds an integer and sign of the rational number is identified here;
+     * -Denominator holds a positive integer;
+     * ---Signs are dealt accordingly in the constructor;
+     */
     private int numerator;
     private int denominator;
 
+    /*
+     * -If denominator = 0, we fix it to arbitrary number, here it was used as 1;
+     * -Signs are issued as wished above; (See the comments above the instance variables)
+     * -Numerator and denomnator are then simplified to their absolute form by dividing
+     * them with their gratest common divisor;
+     */
     public RationalNumber(int numerator, int denominator){
         if(denominator == 0){
             this.denominator = 1;
@@ -23,6 +34,10 @@ public class RationalNumber{
 
     }
 
+    /*
+     * -This private method finds the greatest common divisor of the numerator and
+     * the denominator of a given Rational Number by using Euclidean Algorithm;
+     */
     private int greatestCommonDivisor(RationalNumber rationalMain){
         int min = Math.abs(Math.min(rationalMain.getNumerator(), rationalMain.getDenominator()));
         int max = Math.abs(Math.max(rationalMain.getNumerator(), rationalMain.getDenominator()));
@@ -38,6 +53,9 @@ public class RationalNumber{
 
     }
 
+    /*
+     * -Below are listed getter methods for the instance variables 
+     */
     public int getNumerator(){
         return this.numerator;
 
@@ -48,6 +66,9 @@ public class RationalNumber{
         
     }
 
+    /*
+     * -toString method to showcase the Rational Number;
+     */
     public String toString(){
         return this.numerator + "/" + this.denominator;
 
