5cb676939d03002f07b183c6eb613285a8413ba0
ErenOzilgili <erenozilgili@gmail.com>
2024-02-09 14:37:43 +0300

    trying branching on featureTrial branch

--- Tester.java
+++ Tester.java
@@ -1,7 +1,7 @@
 public class Tester{
 
     public static void main(String[] args) {
-        
+        //
         RationalNumber r1 = new RationalNumber(10, -6);
         System.out.println(r1);
 
